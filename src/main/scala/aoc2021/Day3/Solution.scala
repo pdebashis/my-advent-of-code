c46a8b74de9974482598c98f0a52e03b98edd239
D P <pdebashis@outlook.com>
2025-01-11 23:26:37 +0530

    Very happy with 2021 day4 scala solution

--- src/main/scala/aoc2021/Day3/Solution.scala
+++ src/main/scala/aoc2021/Day3/Solution.scala
@@ -1,8 +1,14 @@
 package aoc2021.Day3
 
+import scala.annotation.tailrec
 import scala.io.Source
 
 class Day3 {
+
+  def toDecimal(str: String) = {
+    str.reverse.zipWithIndex.map{ case (c,i) => if(c=='1') Math.pow(2,i) else 0 }.sum
+  }
+
   def run(inputFile: String) :Unit =  {
     val startTime = System.nanoTime()
     println(s"Executing for ${inputFile.split("/").last}")
@@ -13,8 +19,62 @@ class Day3 {
       source.close()
     }
 
-    val output1 = 0
-    val output2 = 0
+    val selectCommon = input
+      .map( x => x.toCharArray)
+      .transpose
+      .map( cArray => cArray.count(_ == '1'))
+      .map( count => if(count>input.size/2) '1' else '0')
+      .mkString
+
+    val selectUnCommon = input
+      .map( x => x.toCharArray)
+      .transpose
+      .map( cArray => cArray.count(_ == '1'))
+      .map( count => if(count>input.size/2) '0' else '1')
+      .mkString
+
+    val gammaRate = toDecimal(selectCommon)
+    val epsilonRate = toDecimal(selectUnCommon)
+
+    @tailrec
+    def mostCommon( bins :List[String], pos: Int) : String = {
+      if(bins.size == 1) bins.head
+      else{
+        val bits = bins.map( x=> x.charAt(pos))
+        val count0 = bits.count(_ == '0')
+        val count1 = bits.count(_ == '1')
+        val common = count1.compare(count0) match {
+          case 1 => '1'
+          case 0 => '1'
+          case -1 => '0'
+        }
+        val newBins = bins.filter(x => x.charAt(pos) == common)
+        mostCommon(newBins, pos+1)
+      }
+    }
+
+    @tailrec
+    def leastCommon( bins :List[String], pos: Int) : String = {
+      if(bins.size == 1) bins.head
+      else{
+        val bits = bins.map( x=> x.charAt(pos))
+        val count0 = bits.count(_ == '0')
+        val count1 = bits.count(_ == '1')
+        val uncommon = count1.compare(count0) match {
+          case 1 => '0'
+          case 0 => '0'
+          case -1 => '1'
+        }
+        val newBins = bins.filter(x => x.charAt(pos) == uncommon)
+        leastCommon(newBins, pos+1)
+      }
+    }
+
+    val oxyRate = toDecimal(mostCommon(input, 0))
+    val co2Rate = toDecimal(leastCommon(input, 0))
+
+    val output1 = (gammaRate * epsilonRate).toLong
+    val output2 = (oxyRate * co2Rate).toLong
 
     println(output1)
     println(output2)
@@ -30,6 +90,6 @@ object Day3 {
     val app = new Day3()
 
     app.run("./src/main/scala/aoc2021/Day3/Example.txt")
-//    app.run("./src/main/scala/aoc2021/Day3/Input.txt")
+    app.run("./src/main/scala/aoc2021/Day3/Input.txt")
   }
 }
--- src/main/scala/aoc2021/Day4/Solution.scala
+++ src/main/scala/aoc2021/Day4/Solution.scala
@@ -2,6 +2,22 @@ package aoc2021.Day4
 
 import scala.io.Source
 
+class Board(rows_str :List[String], nums: Array[Int]) {
+
+  override def toString :String = {
+    rows.mkString
+  }
+
+  val rows = rows_str.map(_.strip.split("\\s+")).map(row => row.map(_.toInt))
+  val ordering = rows.map(_.map( elem => (elem, nums.indexOf(elem))))
+  val bingo_horizontal = ordering.map(row => row.maxBy{ case(_, order) => order}).minBy{ case(_, order) => order}
+  val bingo_vertical = ordering.transpose.map(row => row.maxBy{ case(_, order) => order}).minBy{ case(_, order) => order}
+  val first_bingo = if(bingo_horizontal._2 < bingo_vertical._2) bingo_horizontal else bingo_vertical
+  val sum_unmarked = ordering.map(row => row.filter{ case(elem, order) => order > first_bingo._2}.map(_._1).sum).sum
+  val score = first_bingo._1 * sum_unmarked
+
+}
+
 class Day4 {
   def run(inputFile: String) :Unit =  {
     val startTime = System.nanoTime()
@@ -13,8 +29,13 @@ class Day4 {
       source.close()
     }
 
-    val output1 = 0
-    val output2 = 0
+    val numbers = input.head.strip.split(",").map(_.toInt)
+    val boards = input.drop(1).filterNot(_.isBlank).grouped(5).toList.map(new Board(_,numbers))
+    val winning_board = boards.minBy(b => b.first_bingo._2)
+    val last_board = boards.maxBy(b => b.first_bingo._2)
+
+    val output1 = winning_board.score
+    val output2 = last_board.score
 
     println(output1)
     println(output2)
@@ -30,6 +51,6 @@ object Day4 {
     val app = new Day4()
 
     app.run("./src/main/scala/aoc2021/Day4/Example.txt")
-//    app.run("./src/main/scala/aoc2021/Day4/Input.txt")
+    app.run("./src/main/scala/aoc2021/Day4/Input.txt")
   }
 }
