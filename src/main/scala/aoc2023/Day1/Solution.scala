8bb0c72265ec6b164ccf2116c786cb6738f7f610
D P <pdebashis@outlook.com>
2025-02-13 20:54:20 +0530

    2023 Day1 solution

--- src/main/scala/aoc2023/Day1/Solution.scala
+++ src/main/scala/aoc2023/Day1/Solution.scala
@@ -13,8 +13,17 @@ class Day1 {
       source.close()
     }
 
-    val output1 = 0
-    val output2 = 0
+    val output1 = input.map(_.toCharArray.filter(_.isDigit).toList).map( l => if(l.isEmpty) "0" else l.head.toString + l.last.toString).map(_.toInt).sum
+    val output2 = input.map(_.replaceAll("one","one1one"))
+      .map(_.replaceAll("two","two2two"))
+      .map(_.replaceAll("three","three3"))
+      .map(_.replaceAll("four","4"))
+      .map(_.replaceAll("five","5five"))
+      .map(_.replaceAll("six","6"))
+      .map(_.replaceAll("seven","7"))
+      .map(_.replaceAll("eight","eight8eight"))
+      .map(_.replaceAll("nine","9nine"))
+      .map(_.toCharArray.filter(_.isDigit).toList).map( l => if(l.isEmpty) "0" else l.head.toString + l.last.toString).map(_.toInt).sum
 
     println(output1)
     println(output2)
@@ -30,6 +39,6 @@ object Day1 {
     val app = new Day1()
 
     app.run("./src/main/scala/aoc2023/Day1/Example.txt")
-//    app.run("./src/main/scala/aoc2023/Day1/Input.txt")
+    app.run("./src/main/scala/aoc2023/Day1/Input.txt")
   }
 }
